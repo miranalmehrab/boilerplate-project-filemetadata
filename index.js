6ffd9c2f781f3a8c917726285ca69edd7f4637d7
miranalmehrab <miranalmehrab@protonmail.com>
2024-03-30 20:01:27 +0600

    feature: add multer package to process file uploads

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
+var upload = multer();
 var app = express();
 
 app.use(cors());
@@ -11,7 +13,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+    res.json({name: req.file.originalname, "type": req.file.mimetype, "size": req.file.size});
+});  
 
 
 const port = process.env.PORT || 3000;
